5bca3249702dbbd0b73fe728a06957e04bf20f40
gizelegabriele <gizelegabriele@gmail.com>
2025-03-18 01:06:37 -0300

    feat:coordenador e relacionamentos

--- AvaliacaoPrimeiraFase/models/turma.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require("mongoose");
-
-let turmaSchema = new mongoose.Schema({
-  nome: { type: String, required: true },
-  alunos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Aluno" }],
-  professor: { type: mongoose.Schema.Types.ObjectId, ref: "Professor" }
-});
-
-module.exports = mongoose.model("Turma", turmaSchema);
